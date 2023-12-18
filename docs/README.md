488f09453f8644ff8422e5c30af61e9a982741c2
mung96 <mung5079@gmail.com>
2023-12-18 10:14:55 +0900

    feat:Select Tip 선택하면 색상변화(아직 다중선택됨)

--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,6 @@
 # 기능구현목록
-[o]Select Tip에 마우스를 가져가면 색상이 변화
-[]Select Tip 클릭하면 색상 변화
+[x]Select Tip에 마우스를 가져가면 색상이 변화
+[x]Select Tip 클릭하면 색상 변화
 []Select Tip 클릭 상태에서 다른 하나 클릭하면 그걸로 색상이 옮겨감
 []Bill에 숫자 적는 기능
 []Number of People에 숫자 적는 기능
--- input.js
+++ input.js
@@ -6,11 +6,17 @@
     
     for(let i=0;i<rates.length;i++){
         let currentRate = rates[i];
+        let currentBackColor = 'rgb( 0, 73, 77)';
         rates[i].addEventListener('mouseover',()=>{
+            currentBackColor = window.getComputedStyle(currentRate).backgroundColor;
             currentRate.style.backgroundColor=SELECTED_COLOR;
         })
         rates[i].addEventListener('mouseout',()=>{
-            currentRate.style.backgroundColor=INIT_COLOR;
+            currentRate.style.backgroundColor=currentBackColor;
+        })
+        rates[i].addEventListener('click',()=>{
+            currentRate.style.backgroundColor=SELECTED_COLOR;
+            currentBackColor = SELECTED_COLOR;
         })
     }
 
